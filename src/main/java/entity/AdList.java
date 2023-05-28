66a9f10b4b2d08195f87db1a65f08c785cb12ddd
z1oti <savchuk.zlata2003@gmail.com>
2023-05-29 00:12:51 +0300

    initial commit

---
+++ src/main/java/entity/AdList.java
@@ -0,0 +1,30 @@
+package entity;
+
+import java.util.HashSet;
+
+public class AdList extends ListOfIdentifiables<Ad> {
+
+    private static final long serialVersionUID = 882150501461356499L;
+
+    public synchronized Ad addAd(User author, Ad ad){
+        ad.setAuthorId(author.getId());
+        ad.setAuthor(author);
+        ad.setId(getNextId());
+
+        items.add(ad);
+        return ad;
+    }
+
+    public synchronized void updateAd(Ad ad){
+        items.add(ad);
+    }
+
+    public synchronized void deleteAd(Ad ad){
+        items.remove(ad);
+    }
+
+    public synchronized HashSet<Ad>getAds(){
+        return (HashSet<Ad>)items.clone();
+    }
+
+}
---
+++ src/main/java/entity/UserExistsException.java
@@ -0,0 +1,5 @@
+package entity;
+
+public class UserExistsException extends Exception {
+    private static final long serialVersionUID = 584737643480913385L;
+}
